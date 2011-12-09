52943d79194a3c33d3c51615746a587977da2c00
Stefano DAmico <skdamico@gmail.com>
2011-12-08 23:31:51 -0500

    Added editing of content, toolbar

--- app/View/Elements/workspace.ctp
+++ app/View/Elements/workspace.ctp
@@ -1,11 +1,27 @@
-<?php $this->Html->script(array('jquery', 'jquery-ui', 'workspace'), array( 'inline' => false )); ?>
-<?php $this->Html->css(array('flick/jquery-ui', 'styles.workspace'), null, array( 'inline' => false )); ?>
+<?php $this->Html->script(array('jquery', 'jquery-ui', 'workspace'), array( 'inline' => false, 'once' => true )); ?>
+<?php $this->Html->css(array('flick/jquery-ui', 'styles.workspace'), null, array( 'inline' => false, 'once' => true )); ?>
 
 <?php $posts = $this->requestAction('posts/get_by_workspace/'. $workspace_id); ?>
 
 <div id='workspace-container'>
     <div class='workspace-tools'>
-        <div class='post tool'><p class='content'></p></div>
+        <div class='post tool'>
+            <div class='content'></div>
+            <ul class='options' style='display: none;'>
+                <li id='edit'>
+                    <span class='option-icon ui-icon ui-icon-pencil'></span>
+                    <span class='option-text'>Edit</span>
+                </li>
+                <li id='color'>
+                    <span class='option-icon ui-icon ui-icon-copy'></span>
+                    <span class='option-text'>Color</span>
+                </li>
+                <li id='delete'>
+                    <span class='option-icon ui-icon ui-icon-trash'></span>
+                    <span class='option-text'>Delete</span>
+                </li>
+            </ul>
+        </div>
         <div class='trash tool'>
             <div class='lid'></div>
             <div class='body'></div>
@@ -15,7 +31,24 @@
         <?php foreach( $posts as $post ): ?>
             <div class='post item' data-id='<?php echo $post['Post']['id']; ?>' 
                   style='position: absolute; top: <?php echo $post['Post']['position_y']; ?>px; left: <?php echo $post['Post']['position_x']; ?>px'>
-                <p class='content'><?php echo $post['Post']['content']; ?></p>
+                <div class='content'>
+                    <div class='display'><?php echo $post['Post']['content']; ?></div>
+                    <div class='char-count'></div>
+                </div>
+                <ul class='options'>
+                    <li id='edit'>
+                        <span class='option-icon ui-icon ui-icon-pencil'></span>
+                        <span class='option-text'>Edit</span>
+                    </li>
+                    <li id='color'>
+                        <span class='option-icon ui-icon ui-icon-copy'></span>
+                        <span class='option-text'>Color</span>
+                    </li>
+                    <li id='delete'>
+                        <span class='option-icon ui-icon ui-icon-trash'></span>
+                        <span class='option-text'>Delete</span>
+                    </li>
+                </ul>
             </div>
         <?php endforeach; ?>
     </div>
--- app/webroot/js/workspace.js
+++ app/webroot/js/workspace.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
     var workspaceId = $("#workspace-id").val();
+    var timeout = 0;
+    var optionsVisible = true;
 
 
+    function limitChars($text, limit, $count) {
+        var text = $text.val();
+        var textlength = text.length;
 
-    function initWorkspaceDrag(selector) {
+        if(textlength > limit) {
+            $text.val(text.substr(0,limit));
+            $count.html($text.val().length + '/' + limit);
+            return false;
+        }
+        else {
+            $count.html($text.val().length + '/' + limit);
+            return true;
+        }
+    }
+
+    function initPost(selector) {
         selector.draggable({
             revert: function(droppable) {
                 if(droppable === false) {
@@ -21,12 +37,90 @@ $(document).ready(function() {
             },
             cursor: "move",
             stack: ".item",
-            cursorAt: { cursor: "move", top: 70, left: 70 }
+            cursorAt: { cursor: "move", top: 70, left: 70 },
+            start: function(event, ui) {
+                optionsVisible = false;
+            },
+            stop: function(event, ui) {
+                optionsVisible = true;
+            }
+        });
+        
+        selector.hover(
+            function() {
+                if(timeout != 0) clearTimeout(timeout);
+                
+                var $post = this;
+                timeout = setTimeout(function() {
+                    timeout = 0;  
+                    
+
+                    if(optionsVisible) {
+                        $('.options', $post).slideDown(200);
+                    }
+                },
+                500);
+            },
+            function() {
+                if(timeout != 0) clearTimeout(timeout);
+
+                var $post = this;
+                
+                timeout = setTimeout(function() {
+                    timeout = 0;
+                    $(".options", $post).slideUp(200);
+                },
+                400);
+            }
+        );
+
+        // initialize toolbar options
+        $('.options #edit', selector).click(function() {
+            // add input box to post-it
+            // on blur, save
+            var $this_element = $(this).parent().parent();
+            var $content = $('.content .display', $this_element);
+
+            var post_content = $content.html();
+            $content.html("<textarea class='temp'></textarea>");
+            var $tmp_ta = $('.temp', $content);
+            $tmp_ta.val(post_content);
+
+            $tmp_ta.keyup(function() {
+                limitChars($tmp_ta, 140, $('.content .char-count', $this_element));
+            });
+            $tmp_ta.focus();
+
+            $tmp_ta.blur(function() {
+                var new_content = $(this).val();
+                $.post("/stickyspaces/posts/update/"+$this_element.attr('data-id'), {
+                    "data[Post][content]": new_content
+                },
+                function(data) {
+                    $content.html(new_content);
+                    $('.content .char-count', $this_element).html("");
+                });
+            });
+        });
+
+        $('.options #color', selector).click(function() {
+            // allow user to pick and preview color for post
+            // pick out of 6 to 8 colors
+        });
+
+        $('.options #delete', selector).click(function() {
+            var $this_element = $(this).parent().parent();
+
+            $.post("/stickyspaces/posts/remove/"+$this_element.attr('data-id'), null, function(data) {
+                $this_element.effect("fold", {}, 500, function() { $this_element.remove(); });
+            });
         });
     }
 
+    // initialize any prepopulated posts
+    initPost($(".workspace-content .item"));
 
-    $(".workspace-tools .tool").draggable({ 
+    $(".workspace-tools .post").draggable({ 
         revert: "invalid",
         helper: "clone",
         cursor: "move",
@@ -44,11 +138,11 @@ $(document).ready(function() {
         tolerance: "fit",
         drop: function(event, ui) {
             // new tool!
-            if($(ui.helper).hasClass("tool")) {
-                var posLeft = ui.helper.position().left - $(this).offset().left;
-                var posTop = ui.helper.position().top - $(this).offset().top;
+            if(ui.helper.hasClass("tool")) {
+                var posLeft = ui.helper.offset().left - $(this).offset().left;
+                var posTop = ui.helper.offset().top - $(this).offset().top;
 
-                $(this).append($(ui.helper).clone().
+                $(this).append(ui.helper.clone().
                     removeClass("ui-draggable-dragging").
                     css({"position": "absolute", "left": posLeft, "top": posTop }));
 
@@ -70,7 +164,7 @@ $(document).ready(function() {
                             $currentItem.attr('data-id', data); 
                             $currentItem.addClass("item");
                             $currentItem.removeClass("ui-draggable tool");
-                            initWorkspaceDrag($currentItem);
+                            initPost($currentItem);
                         }
                 );
             }
@@ -91,6 +185,4 @@ $(document).ready(function() {
         }
     });
 
-    // initialize any prepopulated posts
-    initWorkspaceDrag($(".workspace-content .item"));
 });
